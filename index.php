4714a7a2ec02b4ec17a422941993a8fde0865b43
Ashutosh Dubey <idkashutosh@gmail.com>
2019-09-13 11:53:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname = "shop";
+$username = "tanushree";
+$password = "pankaj12!@";
+$dbname = "rentnjoy";
 $reqdata = json_decode(file_get_contents("php://input"),true);
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
