ff0ca186c5dbc84c5c74992a7d32cf19deb3ff35
Ashutosh Dubey <idkashutosh@gmail.com>
2019-09-20 22:29:03 +0530

    added database file

--- addtocart.php
+++ addtocart.php
@@ -9,9 +9,10 @@ if($reqdata["apikey"] == "someapikey"){
   $user4 = $reqdata["user4"];
   $user5 = $reqdata["user5"];
   $pid = $reqdata["product_id"];
+  $uid = $reqdata["user_id"];
 
-  $sql = "INSERT INTO cart (user1, user2, user3,user4,user5,product_id,payment_status,admin_verification)
-  VALUES ($user1.$user2,$user3,$user4,$user5,$pid,0,0)";
+  $sql = "INSERT INTO cart (user1, user2, user3,user4,user5,product_id,user_id)
+  VALUES ($user1.$user2,$user3,$user4,$user5,$pid,$uid)";
 if (mysqli_query($conn, $sql)) {
   $last_id = mysqli_insert_id($conn);
   echo '{ "message" : "Added to cart", "type": "success", "id":'.$last_id.'}';
--- pay.php
+++ pay.php
@@ -4,7 +4,7 @@ include_once 'razorpay/Razorpay.php';
 
   if($reqdata["apikey"] == "someapikey"){
     $order  = $client->order->create([
-        'receipt'         => $reqdata["receipt"],
+        'receipt'         => 'receipt_1',
         'amount'          => $reqdata["amount"], // amount in the smallest currency unit
         'currency'        => 'INR',// <a href="https://razorpay.freshdesk.com/support/solutions/articles/11000065530-what-currencies-does-razorpay-support" target="_blank">See the list of supported currencies</a>.)
         'payment_capture' =>  '0'
